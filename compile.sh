1b8e4c97044849cd161aef8312e70638b54a6a22
Sebastian Esp Nielsen <sebastianesp@hotmail.com>
2020-02-07 15:48:54 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,13 +1,7 @@
 SHELL=/bin/bash
 
-curl https://api.csswg.org/bikeshed/ \
-		       --output index.html \
-		       --write-out "%{http_code}" \
-		       --header "Accept: text/plain, text/html" \
-		       -F die-on=nothing \
-		       -F file=@index.bs) && \
-[[ "$$HTTP_STATUS" -eq "200" ]]) || ( \
-	echo ""; cat index.html; echo ""; \
-	rm -f index.html; \
-	exit 22 \
+if curl https://api.csswg.org/bikeshed/ -F file=@index.bs -F force=1 -F die-on=nothing > index.html
+then echo "Bikeshed Processing Successful"
+else echo "Bikeshed Processing Failed"
+fi
 
