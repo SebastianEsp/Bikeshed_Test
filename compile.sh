7c3e9befeb666413ceea5947b925b5473d4bf298
Sebastian Esp Nielsen <sebastianesp@hotmail.com>
2020-02-07 15:36:57 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,28 +1,13 @@
 SHELL=/bin/bash
 
-local: index.bs
-	bikeshed --die-on=nothing spec index.bs
+curl https://api.csswg.org/bikeshed/ \
+		       --output index.html \
+		       --write-out "%{http_code}" \
+		       --header "Accept: text/plain, text/html" \
+		       -F die-on=nothing \
+		       -F file=@index.bs) && \
+[[ "$$HTTP_STATUS" -eq "200" ]]) || ( \
+	echo ""; cat index.html; echo ""; \
+	rm -f index.html; \
+	exit 22 \
 
-index.html: index.bs
-	@ (HTTP_STATUS=$$(curl https://api.csswg.org/bikeshed/ \
-	                       --output index.html \
-	                       --write-out "%{http_code}" \
-	                       --header "Accept: text/plain, text/html" \
-	                       -F die-on=nothing \
-	                       -F file=@index.bs) && \
-	[[ "$$HTTP_STATUS" -eq "200" ]]) || ( \
-		echo ""; cat index.html; echo ""; \
-		rm -f index.html; \
-		exit 22 \
-	);
-
-remote: index.html
-
-ci: index.bs
-	mkdir -p out
-	make remote
-	mv index.html out/index.html
-
-clean:
-	rm index.html
-	rm -rf out
