81a6414cedb5abe5457de7643a980fbfda516221
Sebastian Esp Nielsen <sebastianesp@hotmail.com>
2020-02-07 14:58:47 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -61,6 +61,8 @@ git status
 git add index.html
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
+ls -l
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
